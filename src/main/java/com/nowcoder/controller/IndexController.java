8f43ca96d5c8cc5c96beb52823658602b807517c
zh <zhaohan6357@163.com>
2018-03-11 19:32:44 +0800

    project

--- src/main/java/com/nowcoder/controller/IndexController.java
+++ src/main/java/com/nowcoder/controller/IndexController.java
@@ -1,8 +1,7 @@
 package com.nowcoder.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
@@ -11,9 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class IndexController {
-    @RequestMapping("/")
+    @RequestMapping(path={"/","/index"})
     @ResponseBody
     public String index(){
         return "Hello world";
     }
+    @RequestMapping(path={"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("userId") int userId,
+                          @PathVariable("groupId") String groupId,
+                          @RequestParam(value="type",defaultValue = "1",required = false) int type,
+                          @RequestParam(value = "key",defaultValue  ="kk",required = false) String key){
+        return String.format("Profile Group of %s, Page of %d,t:%d,k:%s",groupId,userId,type,key);
+    }
+    @RequestMapping(path={"/template"})
+    public String template(){
+        return "home";
+    }
+
+
+
 }
